12334778c8c84a642ada9ce7fc90185891c14a7f
Graham Fairweather <xotic750@gmail.com>
2019-07-16 23:23:08 +0200

    :bookmark: v2.0.7
    Auto build

--- dist/is-array-x.js
+++ dist/is-array-x.js
@@ -2,13 +2,13 @@
 {
   "author": "Graham Fairweather",
   "copywrite": "Copyright (c) 2017-present",
-  "date": "2019-07-16T10:46:55.639Z",
+  "date": "2019-07-16T21:23:03.413Z",
   "describe": "",
   "description": "Determines whether the passed value is an Array.",
   "file": "is-array-x.js",
   "hash": "3d7deccdfbfa5f122b5b",
   "license": "MIT",
-  "version": "2.0.6"
+  "version": "2.0.7"
 }
 */
 (function webpackUniversalModuleDefinition(root, factory) {
