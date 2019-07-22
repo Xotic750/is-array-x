26f667216575d2f667f72fc8dec9315c6d2a22c9
Graham Fairweather <xotic750@gmail.com>
2019-07-22 14:37:03 +0200

    :bookmark: v2.0.15
    Dependency updates

--- dist/is-array-x.js
+++ dist/is-array-x.js
@@ -2,13 +2,13 @@
 {
   "author": "Graham Fairweather",
   "copywrite": "Copyright (c) 2017-present",
-  "date": "2019-07-21T22:39:12.294Z",
+  "date": "2019-07-22T12:36:58.396Z",
   "describe": "",
   "description": "Determines whether the passed value is an Array.",
   "file": "is-array-x.js",
   "hash": "85ece551f682f42a388f",
   "license": "MIT",
-  "version": "2.0.14"
+  "version": "2.0.15"
 }
 */
 (function webpackUniversalModuleDefinition(root, factory) {
